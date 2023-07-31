c8d1a0ea90ab9b41179cea48adf4a0fbe8f0062a
mederian <ole.snekkermo@gmail.com>
2023-07-31 11:25:12 +0200

    Setup comments on tasks todo in etch.js

---
+++ etch.js
@@ -0,0 +1,18 @@
+
+
+//a div that holds the grid:
+const gridholder = document.createElement('div');
+
+// Create 16x16=256 divs for the grid (This should be changeable, 
+// so number of grid can be changed, but size of the containergrid is the same.)
+// So size of the grid elements should be flexible.. 
+
+// The divs should appear in a grid, use flexbox
+
+// Add mouseEnter and mouseLeave listener event for each of the divs. 
+// function connected to the eventlistener will change the colour of the grid element
+
+// Border and margins can mess up the grid
+
+
+
